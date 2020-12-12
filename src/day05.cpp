418f7d9a51b4550e75776b7daddfdbb2a172bbd3
yanto <yanto@iki.fi>
2020-12-12 14:29:34 +0200

    [day05] add 2nd part solution

--- src/day05.cpp
+++ src/day05.cpp
@@ -26,21 +26,72 @@ namespace
     {
         return row * 8 + col;
     }
+
+    std::pair<uint8_t, uint8_t> get_row_col(uint16_t seat_id)
+    {
+        return { seat_id / 8, seat_id % 8 };
+    }
 }
 
 void day05(const input_t& input)
 {
     uint16_t max_seat_id = 0; // Part 1
+    uint16_t my_seat_id = 0; // Part 2
+
+    constexpr uint8_t MAX_ROW = 1 << 7;
+    constexpr uint8_t MAX_COL = 1 << 3;
+    std::array<std::array<bool, MAX_COL>, MAX_ROW> seats;
+    for (auto& row: seats)
+    {
+        row.fill(false);
+    }
+
     parse_input(input, [&](const std::string_view& line)
     {
         const auto& [row, col] = convert_line(line);
         uint16_t seat_id = get_seat_id(row, col);
+
+        // Part 1
         if (seat_id > max_seat_id)
             max_seat_id = seat_id;
+
+        // Part 2
+        seats[row][col] = true;
     });
 
-    printf("%d", max_seat_id);
+
+    // Part 2
+    for (uint8_t row = 0; row < MAX_ROW; ++row)
+    {
+        for (uint8_t col = 0; col < MAX_COL; ++col)
+        {
+            if (seats[row][col] == false)
+            {
+                const uint16_t seat_id = get_seat_id(row, col);
+
+                // Your seat is in the middle of the plane: the seats with
+                // IDs +1 and -1 from yours will be [taken] in your list.
+                const auto& [n_row, n_col] = get_row_col(seat_id + 1);
+                const auto& [p_row, p_col] = get_row_col(seat_id - 1);
+                const bool prev_seat_taken = seats[n_row][n_col];
+                const bool next_seat_taken = seats[p_row][p_col];
+                if (prev_seat_taken && next_seat_taken)
+                {
+                    my_seat_id = seat_id;
+                    break;
+                }
+            }
+        }
+
+        if (my_seat_id != 0)
+        {
+            break;
+        }
+    }
+
+    printf("max: %d, my: %d", max_seat_id, my_seat_id);
     assert(max_seat_id == 953);
+    assert(my_seat_id == 615);
 }
 
 namespace // tests
