45a0a1943adc5cae24d5583b120d39b8980b30f8
yanto <yanto@iki.fi>
2020-12-12 13:51:57 +0200

    [day05] add solution to part1

--- src/day05.cpp
+++ src/day05.cpp
@@ -21,12 +21,30 @@ namespace
 
         return {row, col};
     }
+
+    uint16_t get_seat_id(uint8_t row, uint8_t col)
+    {
+        return row * 8 + col;
+    }
 }
 
 void day05(const input_t& input)
 {
-    printf("todo");
+    uint16_t max_seat_id = 0; // Part 1
+    parse_input(input, [&](const std::string_view& line)
+    {
+        const auto& [row, col] = convert_line(line);
+        uint16_t seat_id = get_seat_id(row, col);
+        if (seat_id > max_seat_id)
+            max_seat_id = seat_id;
+    });
 
+    printf("%d", max_seat_id);
+    assert(max_seat_id == 953);
+}
+
+namespace // tests
+{
     static_assert(convert_line("BFFFBBFRRR") == std::pair<uint8_t, uint8_t>{70, 7});
     static_assert(convert_line("FFFBBBFRRR") == std::pair<uint8_t, uint8_t>{14, 7});
     static_assert(convert_line("BBFFBBFRLL") == std::pair<uint8_t, uint8_t>{102, 4});
