efaa018ac531d90a39274f91d1d44d6f8a509cef
yanto <yanto@iki.fi>
2020-12-23 19:06:30 +0200

    [day10] wip

--- src/helpers/print_helpers.h
+++ src/helpers/print_helpers.h
@@ -2,11 +2,22 @@
 #include <iostream>
 
 template<typename T>
-void print(const std::vector<T>& input)
+void print_col(const std::vector<T>& input)
 {
     std::cout << "Vector:\n";
     for (const auto& d: input)
     {
-        std::cout << "  " << d << '\n';
+        std::cout << "  " << static_cast<int>(d) << '\n';
     }
+}
+
+template<typename T>
+void print_row(const std::vector<T>& input)
+{
+    std::cout << "Vector:\n";
+    for (const auto& d: input)
+    {
+        std::cout << "  " << static_cast<int>(d) << ", ";
+    }
+    std::cout << '\n';
 }
