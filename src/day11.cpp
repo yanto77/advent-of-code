381f7cda940604c329a5db61255e811b91e9ff09
yanto <yanto@iki.fi>
2020-12-29 14:08:03 +0200

    [day11] add bruteforce solution for part2

--- src/day11.cpp
+++ src/day11.cpp
@@ -113,21 +113,59 @@ namespace
     size_t get_occupied_seats_pt1(const seat_map_t& input, size_t cell_x, size_t cell_y)
     {
         size_t occupied_nb = 0;
-        for (size_t wy = 0; wy < 3; ++wy)
+        for (int wy = -1; wy <= 1; ++wy)
         {
-            for (size_t wx = 0; wx < 3; ++wx)
+            for (int wx = -1; wx <= 1; ++wx)
             {
-                if (wy == 1 && wx == 1)
+                if (wy == 0 && wx == 0)
                     continue; // skip center
 
-                if (input[cell_y-1+wy][cell_x-1+wx] == seat_state_t::TAKEN)
+                if (input[cell_y+wy][cell_x+wx] == seat_state_t::TAKEN)
                     ++occupied_nb;
             }
         }
         return occupied_nb;
     }
 
-    std::pair<size_t, seat_map_t> simulate_until_stable(const seat_map_t& input)
+    size_t get_occupied_seats_pt2(const seat_map_t& input, size_t cell_x, size_t cell_y)
+    {
+        size_t occupied_nb = 0;
+
+        const std::array<vec2i, 8> directions =
+        {
+            vec2i{-1,  1}, vec2i{0,  1}, vec2i{1,  1},
+            vec2i{-1,  0},               vec2i{1,  0},
+            vec2i{-1, -1}, vec2i{0, -1}, vec2i{1, -1},
+        };
+
+        const size_t height = input.size();
+        const size_t width = input[0].size();
+        for (const auto& dir: directions)
+        {
+            vec2i pos = { static_cast<int>(cell_x), static_cast<int>(cell_y) };
+            pos += dir;
+
+            while (0 <= pos.x && pos.x < width && 0 <= pos.y && pos.y < height)
+            {
+                if (input[pos.y][pos.x] == seat_state_t::TAKEN)
+                {
+                    ++occupied_nb;
+                    break;
+                }
+
+                if (input[pos.y][pos.x] == seat_state_t::EMPTY)
+                {
+                    break;
+                }
+
+                pos += dir;
+            }
+        }
+
+        return occupied_nb;
+    }
+
+    std::pair<size_t, seat_map_t> simulate_until_stable(const seat_map_t& input, bool part1)
     {
         size_t steps = 0;
 
@@ -147,14 +185,20 @@ namespace
                 for (size_t x = padding.x; x < width + padding.x; ++x)
                 {
                     // Find occupied seats from the closest 3x3 window
-                    size_t occupied_nb = get_occupied_seats_pt1(current, x, y);
+                    size_t occupied_nb = part1 ?
+                        get_occupied_seats_pt1(current, x, y) :
+                        get_occupied_seats_pt2(current, x, y);
 
                     // Update state
                     if (current[y][x] == seat_state_t::EMPTY && occupied_nb == 0)
                     {
                         next[y][x] = seat_state_t::TAKEN;
                     }
-                    else if (current[y][x] == seat_state_t::TAKEN && occupied_nb >= 4)
+                    else if (part1 && current[y][x] == seat_state_t::TAKEN && occupied_nb >= 4)
+                    {
+                        next[y][x] = seat_state_t::EMPTY;
+                    }
+                    else if (!part1 && current[y][x] == seat_state_t::TAKEN && occupied_nb >= 5)
                     {
                         next[y][x] = seat_state_t::EMPTY;
                     }
@@ -196,27 +240,33 @@ namespace
 void day11(const input_t& input)
 {
     seat_map_t test_map = parse_input(input);
-    const auto& [steps, after_map] = simulate_until_stable(test_map);
-    size_t occupied = count_occupied(after_map);
-    assert(occupied == 2108);
+    const auto& [steps, after_map] = simulate_until_stable(test_map, true);
+    size_t occupied1 = count_occupied(after_map);
+    assert(occupied1 == 2108);
+
+    const auto& [steps2, after_map2] = simulate_until_stable(test_map, false);
+    size_t occupied2 = count_occupied(after_map2);
+    // printf("occupied2: %zu\n", occupied2);
+    assert(occupied2 == 1897);
 }
 
 void day11_test()
 {
+    char str1[111] = "L.LL.LL.LL\n"
+                    "LLLLLLL.LL\n"
+                    "L.L.L..L..\n"
+                    "LLLL.LL.LL\n"
+                    "L.LL.LL.LL\n"
+                    "L.LLLLL.LL\n"
+                    "..L.L.....\n"
+                    "LLLLLLLLLL\n"
+                    "L.LLLLLL.L\n"
+                    "L.LLLLL.LL\n";
+    input_t test_input { str1, 111 };
+
     {
-        char str1[111] = "L.LL.LL.LL\n"
-                        "LLLLLLL.LL\n"
-                        "L.L.L..L..\n"
-                        "LLLL.LL.LL\n"
-                        "L.LL.LL.LL\n"
-                        "L.LLLLL.LL\n"
-                        "..L.L.....\n"
-                        "LLLLLLLLLL\n"
-                        "L.LLLLLL.L\n"
-                        "L.LLLLL.LL\n";
-        input_t test_input { str1, 111 };
         seat_map_t test_map = parse_input(test_input);
-        const auto& [steps, after_map] = simulate_until_stable(test_map);
+        const auto& [steps, after_map] = simulate_until_stable(test_map, true);
 
         char str2[111] = "#.#L.L#.##\n"
                         "#LLL#LL.L#\n"
@@ -233,5 +283,25 @@ void day11_test()
         assert(expected_map == after_map);
         assert(steps == 5);
         assert(count_occupied(after_map) == 37);
+    }
+    {
+        seat_map_t test_map = parse_input(test_input);
+        const auto& [steps, after_map] = simulate_until_stable(test_map, false);
+
+        char str2[111] = "#.L#.L#.L#\n"
+                        "#LLLLLL.LL\n"
+                        "L.L.L..#..\n"
+                        "##L#.#L.L#\n"
+                        "L.L#.LL.L#\n"
+                        "#.LLLL#.LL\n"
+                        "..#.L.....\n"
+                        "LLL###LLL#\n"
+                        "#.LLLLL#.L\n"
+                        "#.L#LL#.L#\n";
+        input_t expected_input { str2, 111 };
+        seat_map_t expected_map = parse_input(expected_input);
+        assert(expected_map == after_map);
+        assert(steps == 6);
+        assert(count_occupied(after_map) == 26);
     }
 }
