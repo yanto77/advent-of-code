ce12807aaf43ba5af802d6856aa4b87cfd8c9986
yanto <yanto@iki.fi>
2021-01-06 13:34:38 +0200

    [day13] add solution for part2

--- src/day13.cpp
+++ src/day13.cpp
@@ -1,17 +1,14 @@
 #include "advent2020.h"
 
-/// line equation: y = mx + b
-struct line_t
+struct data_t
 {
-    int m = 0;
-    int b = 0;
+    int t0 = 0;
+    std::vector<vec2i> buslines = {};
+};
 
-    friend bool operator==(const line_t& lhs, const line_t& rhs)
-    {
-        return lhs.m == rhs.m && lhs.b == rhs.b;
-    }
-
-    static vec2i intersect(const line_t& lhs, const line_t& rhs)
+namespace
+{
+    vec2i intersect(const vec2i& lhs, const vec2i& rhs)
     {
         auto& [a, c] = lhs;
         auto& [b, d] = rhs;
@@ -25,20 +22,44 @@ struct line_t
 
         return {x, y};
     }
-};
 
-struct data_t
-{
-    int t0 = 0;
-    std::vector<line_t> buslines = {};
-};
+    // Computes a^b (mod m)
+    // Ref: https://cp-algorithms.com/algebra/binary-exp.html
+    int64_t binpow(int64_t a, int64_t b, int64_t m)
+    {
+        a %= m;
+        int64_t res = 1;
+        while (b > 0) {
+            if (b & 1)
+                res = res * a % m;
+            a = a * a % m;
+            b >>= 1;
+        }
+        return res;
+    }
 
-namespace
-{
+    std::vector<vec2i> parse_eq_line(const sv& line)
+    {
+        std::vector<vec2i> eqs;
+
+        eqs.reserve(100);
+        const auto& parts = split_multi(line, ',');
+        for (size_t i = 0; i < parts.size(); ++i)
+        {
+            if (parts[i][0] != 'x')
+            {
+                int m = to_int<int>(parts[i]);
+                int r = static_cast<int>(i);
+                eqs.push_back(vec2i { (m - r) % m, m });
+            }
+        }
+
+        return eqs;
+    }
+
     data_t parse_input(const input_t& input)
     {
         data_t parsed;
-        parsed.buslines.reserve(100);
 
         bool first = true;
         parse_input(input, [&](const sv& line)
@@ -50,17 +71,7 @@ namespace
             }
             else
             {
-                const auto& parts = split_multi(line, ',');
-                for (size_t i = 0; i < parts.size(); ++i)
-                {
-                    if (parts[i][0] != 'x')
-                    {
-                        parsed.buslines.push_back(line_t {
-                            .m = to_int<int>(parts[i]),
-                            .b = -1 * static_cast<int>(i)
-                        });
-                    }
-                }
+                parsed.buslines = parse_eq_line(line);
             }
         });
 
@@ -69,14 +80,16 @@ namespace
 
     std::pair<int, int> evaluate_pt1(const data_t& data)
     {
-        const line_t base{0, data.t0};
+        // Consider input vec2i's to contain {m, b} pairs for
+        // a system of linear equations: y = (m)x + (b),
+        const vec2i base{0, data.t0};
         const int n = static_cast<int>(data.buslines.size());
 
         // Intersect all buslines with expected base-line
         std::vector<int> result { n, 0 };
         for (size_t i = 0; i < n; ++i)
         {
-            const vec2i& point = line_t::intersect(base, { data.buslines[i].m, 0 });
+            const vec2i& point = intersect(base, { data.buslines[i].y, 0 });
             result[i] = point.y;
         }
 
@@ -86,7 +99,39 @@ namespace
             if (data.t0 < result[i] && result[i] < min.first)
                 min = { result[i], i };
 
-        return {(min.first - data.t0), data.buslines[min.second].m};
+        return {(min.first - data.t0), data.buslines[min.second].y};
+    }
+
+    /// Consider input vec2i's to contain {a, m} pairs for
+    /// a system of linear congruences: x = a (mod m)
+    /// Solves for x.
+    int64_t solve_congruence_sys(const std::vector<vec2i>& eqs)
+    {
+        // Ref: https://www.dave4math.com/mathematics/chinese-remainder-theorem/
+        // Ref: https://fasterthanli.me/series/advent-of-code-2020/part-13
+        int64_t res = 0;
+
+        int64_t N = 1;
+        for (const vec2i& eq: eqs) { N *= eq.y; }
+
+        for (const vec2i& eq: eqs)
+        {
+            const auto& [a_i, n_i] = eq;
+            int64_t nh_i = N / n_i;
+
+            // Solve u_i (Modular_multiplicative_inverse) using Euler's theorem
+            // Ref: https://en.wikipedia.org/wiki/Modular_multiplicative_inverse#Using_Euler's_theorem
+            int64_t u_i = binpow(nh_i, n_i-2, n_i) % (n_i);
+
+            // Apply
+            res += (a_i * nh_i * u_i);
+            res %= N;
+        }
+
+        if (res < 0)
+            res += N;
+
+        return res;
     }
 }
 
@@ -95,69 +140,38 @@ output_t day13(const input_t& input)
     data_t data = parse_input(input);
 
     const auto& [diff, bus] = evaluate_pt1(data);
-    printf("diff: %d, bus: %d, result: %d\n", diff, bus, diff*bus);
-
-    return { static_cast<size_t>(diff*bus), 0 };
+    const size_t part1 = diff*bus;
+    const size_t part2 = solve_congruence_sys(data.buslines);
+    return { part1, part2 };
 }
 
 void day13_test()
 {
-    char text[25] = "939\n7,13,x,x,59,x,31,19\n";
-    input_t test1 { text, 25 };
+    // Part 1
+    {
+        char text[25] = "939\n7,13,x,x,59,x,31,19\n";
+        input_t test1 { text, 25 };
 
-    data_t data = parse_input(test1);
-    assert(data.t0 == 939);
-    assert(data.buslines == std::vector<line_t>({
-        line_t {7, 0},
-        line_t {13, -1},
-        line_t {59, -4},
-        line_t {31, -6},
-        line_t {19, -7}
-    }));
+        data_t data = parse_input(test1);
+        assert(data.t0 == 939);
+        assert(data.buslines == std::vector<vec2i>({
+            vec2i {0, 7},
+            vec2i {12, 13},
+            vec2i {55, 59},
+            vec2i {25, 31},
+            vec2i {12, 19}
+        }));
 
-    const auto& [diff, bus] = evaluate_pt1(data);
-    assert(diff == 5 && bus == 59);
+        const auto& [diff, bus] = evaluate_pt1(data);
+        assert(diff == 5 && bus == 59);
+    }
+
+    // Part 2
+    {
+        assert(3417 == solve_congruence_sys(parse_eq_line("17,x,13,19")));
+        assert(754018 == solve_congruence_sys(parse_eq_line("67,7,59,61")));
+        assert(779210 == solve_congruence_sys(parse_eq_line("67,x,7,59,61")));
+        assert(1261476 == solve_congruence_sys(parse_eq_line("67,7,x,59,61")));
+        assert(1202161486 == solve_congruence_sys(parse_eq_line("1789,37,47,1889")));
+    }
 }
-
-/*
-find n, where
-    n = (b[0] * k1) + 0 =  7 * k1 + 0
-    n = (b[1] * k2) + 1 = 13 * k2 + 1
-    n = (b[2] * k3) + 4 = 59 * k3 + 4
-    n = (b[3] * k4) + 6 = 31 * k4 + 6
-    n = (b[4] * k5) + 7 = 19 * k5 + 7
-
-1068781
-    7 * 152683 - 0  = 1068781
-    13 * 82214 - 1  = 1068781
-    59 * 18115 - 4  = 1068781
-    31 * 34477 - 6  = 1068781
-    19 * 56252 - 7  = 1068781
-
-x = 1068781, not really right, is it?
-    x mod 7 = 0
-    x mod 13 = 1
-    x mod 59 = 4
-    x mod 31 = 6
-    x mod 19 = 7
-
-    7 * 152683 - 0  = 1068781
-    13 * 82214 - 1  = 1068781
-    59 * 18115 - 4  = 1068781
-    31 * 34477 - 6  = 1068781
-    19 * 56252 - 7  = 1068781
-
-see also:
-* https://sites.millersville.edu/bikenaga/number-theory/systems-of-congruences/systems-of-congruences.html
-* https://www.reddit.com/r/adventofcode/comments/kc4njx/2020_day_13_solutions/ggv5a0s/?utm_source=reddit&utm_medium=web2x&context=3
-    > I found out that the "Chinese remainder theorem" was involved so I used 
-    > the following videos to gain a grasp of how modular arithmetic and the theorem worked:
-    >    https://youtu.be/5OjZWSdxlU0
-    >    https://youtu.be/6dZLq77gSGU
-    >    https://youtu.be/zIFehsBHB8o
-    > also:
-    > https://www.dcode.fr/chinese-remainder
-
-* https://github.com/blu3r4y/AdventOfLanguages2020/blob/main/src/day13.cpp
-
-*/
