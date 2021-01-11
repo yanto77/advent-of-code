faa6f87b79466380e828f935595caa20db10bfde
yanto <yanto@iki.fi>
2021-01-11 21:37:24 +0200

    update helpers

--- src/day13.cpp
+++ src/day13.cpp
@@ -23,21 +23,6 @@ namespace
         return {x, y};
     }
 
-    // Computes a^b (mod m)
-    // Ref: https://cp-algorithms.com/algebra/binary-exp.html
-    int64_t binpow(int64_t a, int64_t b, int64_t m)
-    {
-        a %= m;
-        int64_t res = 1;
-        while (b > 0) {
-            if (b & 1)
-                res = res * a % m;
-            a = a * a % m;
-            b >>= 1;
-        }
-        return res;
-    }
-
     std::vector<vec2i> parse_eq_line(const sv& line)
     {
         std::vector<vec2i> eqs;
