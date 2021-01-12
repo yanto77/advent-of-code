ee27b4771f7f26e1bd1cc450a317576db7c4a9ca
yanto <yanto@iki.fi>
2021-01-12 22:56:09 +0200

    [day15] update history data struct from map to vector

--- src/day15.cpp
+++ src/day15.cpp
@@ -2,52 +2,46 @@
 
 namespace
 {
-    typedef std::unordered_map<size_t, std::pair<size_t, size_t>> history_t;
+    struct value_t {
+        size_t turn2 = 0;
+        size_t turn1 = 0;
+
+        friend bool operator==(const value_t& lhs, const value_t& rhs) 
+        { 
+            return lhs.turn2 == rhs.turn2 &&  lhs.turn1 == rhs.turn1;
+        }
+    };
 
     size_t solve(const std::vector<size_t>& input, size_t limit)
     {
-        // Turn  1:                              Value=0, Insert={1, 0}
-        // Turn  2:                              Value=3, Insert={2, 0}
-        // Turn  3:                              Value=6, Insert={3, 0}
-        // Turn  4: LastValue=6. History={3, 0}, Value=0, Update={4, 1}
-        // Turn  5: LastValue=0. History={4, 1}, Value=3, Update={5, 2}
-        // Turn  6: LastValue=3, History={5, 2}, Value=3, Update={6, 5}
-        // Turn  7: LastValue=3, History={6, 5}, Value=1, Insert={7, 0}
-        // Turn  8: LastValue=1, History={7, 0}, Value=0, Update={8, 4}
-        // Turn  9: LastValue=0, History={8, 4}, Value=4, Update={9, 0}
-        // Turn 10: LastValue=4, History={9, 0}, Value=0, Update={10, 8}
+        std::vector<value_t> history(limit, value_t{});
 
-        history_t history;
-        history.reserve(limit);
-
-        history[input[0]] = {1, 1};
-        size_t last_value = input[0];
+        // history[input[0]] = {1, 1};
+        size_t value = input[0];
         size_t last_diff = 0;
 
-        for (size_t n = 1; n < limit; ++n)
+        for (size_t n = 0; n < limit; ++n)
         {
             size_t turn = n+1;
-            size_t new_value = (n < input.size()) ? input[n] : last_diff;
+            value = (n < input.size()) ? input[n] : last_diff;
 
-            // printf("turn: %zu, last_value: %zu, history: {%zu, %zu}, value: %zu\n", turn, last_value, 
-            //     history[last_value].first, history[last_value].second, new_value);
+            // printf("turn: %zu, last_value: %zu, history: {%zu, %zu}, value: %zu (%s)\n", turn, last_value, 
+            //     history[last_value].turn2, history[last_value].turn1, new_value, (n < input.size()) ? "input" : "history");
 
-            auto it = history.find(new_value);
-            if (it != history.end())
+            auto it = history[value];
+            if (it != value_t{})
             {
-                last_diff = turn - it->second.first;
-                it->second = { turn, it->second.first };
+                last_diff = turn - it.turn2;
+                history[value] = { turn, it.turn2 };
             }
             else
             {
-                history[new_value] = {turn, turn};
                 last_diff = 0;
+                history[value] = { turn, turn };
             }
-
-            last_value = new_value;
         }
 
-        return last_value;
+        return value;
     }
 }
 
@@ -60,6 +54,18 @@ output_t day15(const input_t& input)
 
 void day15_test()
 {
+    // Turn  1:                              Value=0, Insert={1, 0}
+    // Turn  2:                              Value=3, Insert={2, 0}
+    // Turn  3:                              Value=6, Insert={3, 0}
+    // Turn  4: LastValue=6. History={3, 0}, Value=0, Update={4, 1}
+    // Turn  5: LastValue=0. History={4, 1}, Value=3, Update={5, 2}
+    // Turn  6: LastValue=3, History={5, 2}, Value=3, Update={6, 5}
+    // Turn  7: LastValue=3, History={6, 5}, Value=1, Insert={7, 0}
+    // Turn  8: LastValue=1, History={7, 0}, Value=0, Update={8, 4}
+    // Turn  9: LastValue=0, History={8, 4}, Value=4, Update={9, 0}
+    // Turn 10: LastValue=4, History={9, 0}, Value=0, Update={10, 8}
+    assert(solve({0,3,6}, 10) == 0);
+
     assert(solve({0,3,6}, 2020) == 436);
     assert(solve({1,3,2}, 2020) == 1);
     assert(solve({2,1,3}, 2020) == 10);
@@ -68,7 +74,7 @@ void day15_test()
     assert(solve({3,2,1}, 2020) == 438);
     assert(solve({3,1,2}, 2020) == 1836);
 
-    // NB: Takes approx 1-2 second per each call.
+    // NB: Takes approx 1 second per each call.
     // assert(solve({0,3,6}, 30000000) == 175594);
     // assert(solve({1,3,2}, 30000000) == 2578);
     // assert(solve({2,1,3}, 30000000) == 3544142);
