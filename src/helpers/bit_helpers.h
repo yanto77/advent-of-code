442b983f6757e0784a6194cc091fcf2bb0385883
yanto <yanto@iki.fi>
2021-01-17 14:07:26 +0200

    extend bit helpers with std::vector<bool> methods

--- src/helpers/bit_helpers.h
+++ src/helpers/bit_helpers.h
@@ -36,6 +36,15 @@ constexpr void reset_bitmask(T& trg, const T& mask)
     trg &= ~mask;
 }
 
+inline void reset_bitmask(std::vector<bool>& trg, const std::vector<bool>& mask)
+{
+    size_t size = trg.size();
+    for (size_t i = 0; i < size; ++i)
+    {
+        trg[i] = (trg[i] & ~mask[i]);
+    }
+}
+
 template <typename T>
 constexpr bool get_bit(T num, size_t bit)
 {
@@ -51,6 +60,16 @@ constexpr uint8_t count_bits(T n)
     {
         count += n & 1;
         n >>= 1;
+    }
+    return count;
+}
+
+inline uint8_t count_bits(const std::vector<bool>& bits)
+{
+    uint8_t count = 0;
+    for (bool bit: bits)
+    {
+        count += bit & 1;
     }
     return count;
 }
