daf48204b5435b5533bcf9963c860453919ee5c9
yanto <yanto@iki.fi>
2021-01-25 20:38:58 +0200

    update vector helpers

---
+++ src/helpers/vec3.h
@@ -0,0 +1,59 @@
+#pragma once
+
+template <typename T>
+struct vec3 
+{ 
+    T x;
+    T y;
+    T z;
+
+    vec3(T X = 0, T Y = 0, T Z = 0): x(X), y(Y), z(Z) {}
+
+    constexpr vec3<T>& operator+=(const vec3<T>& rhs) { x += rhs.x; y += rhs.y; z += rhs.z; return *this; }
+    constexpr vec3<T>& operator-=(const vec3<T>& rhs) { x -= rhs.x; y -= rhs.y; z -= rhs.z; return *this; }
+    constexpr vec3<T>& operator/=(const vec3<T>& rhs) { x /= rhs.x; y /= rhs.y; z /= rhs.z; return *this; }
+    constexpr vec3<T>& operator*=(const vec3<T>& rhs) { x *= rhs.x; y *= rhs.y; z *= rhs.z; return *this; }
+
+    constexpr vec3<T>& operator+=(const T rhs) { x += rhs; y += rhs; z += rhs; return *this; }
+    constexpr vec3<T>& operator-=(const T rhs) { x -= rhs; y -= rhs; z -= rhs; return *this; }
+    constexpr vec3<T>& operator/=(const T rhs) { x /= rhs; y /= rhs; z /= rhs; return *this; }
+    constexpr vec3<T>& operator*=(const T rhs) { x *= rhs; y *= rhs; z *= rhs; return *this; }
+
+    constexpr friend vec3<T> operator+(vec3<T> lhs, const vec3<T>& rhs) { lhs += rhs; return lhs; }
+    constexpr friend vec3<T> operator-(vec3<T> lhs, const vec3<T>& rhs) { lhs -= rhs; return lhs; }
+    constexpr friend vec3<T> operator/(vec3<T> lhs, const vec3<T>& rhs) { lhs /= rhs; return lhs; }
+    constexpr friend vec3<T> operator*(vec3<T> lhs, const vec3<T>& rhs) { lhs *= rhs; return lhs; }
+
+    constexpr friend vec3<T> operator+(vec3<T> lhs, const T rhs) { lhs += rhs; return lhs; }
+    constexpr friend vec3<T> operator-(vec3<T> lhs, const T rhs) { lhs -= rhs; return lhs; }
+    constexpr friend vec3<T> operator/(vec3<T> lhs, const T rhs) { lhs /= rhs; return lhs; }
+    constexpr friend vec3<T> operator*(vec3<T> lhs, const T rhs) { lhs *= rhs; return lhs; }
+
+    constexpr friend vec3<T> operator+(const T lhs, vec3<T> rhs) { rhs += lhs; return rhs; }
+    constexpr friend vec3<T> operator-(const T lhs, vec3<T> rhs) { rhs -= lhs; return rhs; }
+    constexpr friend vec3<T> operator/(const T lhs, vec3<T> rhs) { rhs /= lhs; return rhs; }
+    constexpr friend vec3<T> operator*(const T lhs, vec3<T> rhs) { rhs *= lhs; return rhs; }
+
+    bool operator==(const vec3<T>& v) const { return x == v.x && y == v.y && z == v.z; }
+};
+
+namespace std
+{
+    template <typename T>
+    struct hash<vec3<T>>
+    {   
+        // Ref: https://stackoverflow.com/a/17017281
+        size_t operator()( const vec3<T>& v ) const
+        {
+            size_t res = 17;
+            res = res * 31 + hash<T>()( v.x );
+            res = res * 31 + hash<T>()( v.y );
+            res = res * 31 + hash<T>()( v.z );
+            return res;
+        }
+    };
+}
+
+typedef vec3<int> vec3i;
+typedef vec3<float> vec3f;
+typedef vec3<double> vec3d;
