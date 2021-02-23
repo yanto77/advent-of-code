5654ba230402cdc976d93488366195e70f45e399
yanto <yanto@iki.fi>
2021-02-23 23:25:53 +0200

    [day20] small updates

--- src/day20.cpp
+++ src/day20.cpp
@@ -3,6 +3,7 @@
 namespace
 {
     enum side_t { TOP, RIGHT, BOTTOM, LEFT };
+    constexpr std::array<side_t, 4> SIDES = { TOP, RIGHT, BOTTOM, LEFT };
 
     struct tile_t
     {
@@ -97,33 +98,26 @@ namespace
         return tiles;
     }
 
-    bool match_two_sides(const std::array<bool, 10>& side1, const std::array<bool, 10>& side2)
-    {
-        std::array<bool, 10> side1r;
-        std::reverse_copy(std::begin(side1), std::end(side1), std::begin(side1r));
-        return side1 == side2 || side1r == side2;
-    }
-
     void check_two_tiles(std::vector<tile_t>& tiles, size_t t1, size_t t2)
     {
-        constexpr std::array<side_t, 4> sides = { TOP, RIGHT, BOTTOM, LEFT };
-        for (const side_t s1: sides)
+        for (const side_t s1: SIDES)
         {
             if (tiles[t1].side_tiles[s1] != nullptr)
                 continue;
 
-            for (const side_t s2: sides)
+            for (const side_t s2: SIDES)
             {
                 if (tiles[t2].side_tiles[s2] != nullptr)
                     continue;
 
-                if (match_two_sides(tiles[t1].sides[s1], tiles[t2].sides[s2]))
-                {
-                    // printf("comparing tiles: %zu + %zu, sides: %d + %d\n", t1, t2, s1, s2);
-                    // printf(" - "); tiles[t1].print_side(s1);
-                    // printf(" - "); tiles[t2].print_side(s2);
-                    // printf(COLOR_GREEN() " - found match!\n" COLOR_RESET());
+                const auto& side1 = tiles[t1].sides[s1];
+                const auto& side2 = tiles[t2].sides[s2];
 
+                std::array<bool, 10> side1r;
+                std::reverse_copy(std::begin(side1), std::end(side1), std::begin(side1r));
+
+                if (side1 == side2 || side1r == side2)
+                {
                     tiles[t1].side_tiles[s1] = &tiles[t2];
                     tiles[t2].side_tiles[s2] = &tiles[t1];
                     return;
@@ -132,7 +126,7 @@ namespace
         }
     }
 
-    void combine_tiles(std::vector<tile_t>& tiles)
+    std::array<tile_t*, 4> get_corners(std::vector<tile_t>& tiles)
     {
         for (size_t t1 = 0; t1 < tiles.size(); ++t1)
         {
@@ -144,89 +138,31 @@ namespace
                 check_two_tiles(tiles, t1, t2);
             }
         }
-    }
 
-    std::array<const tile_t*, 4> get_corners(const std::vector<tile_t>& tiles)
-    {
-        std::array<const tile_t*, 4> corners {};
+        std::array<tile_t*, 4> corners = { };
         size_t idx = 0;
-
-        for (size_t i = 0; i < tiles.size(); ++i)
+        for (size_t t = 0; t < tiles.size(); ++t)
         {
-            size_t count = 0;
-            count += static_cast<int>(tiles[i].side_tiles[TOP] != nullptr);
-            count += static_cast<int>(tiles[i].side_tiles[RIGHT] != nullptr);
-            count += static_cast<int>(tiles[i].side_tiles[BOTTOM] != nullptr);
-            count += static_cast<int>(tiles[i].side_tiles[LEFT] != nullptr);
+            size_t adjacent_n = 0;
 
-            // printf("tile: %zu\n", tiles[i].id);
-            // printf("  - %zu \n", tiles[i].side_tiles[TOP] ? tiles[i].side_tiles[TOP]->id : 0);
-            // printf("  - %zu \n", tiles[i].side_tiles[RIGHT] ? tiles[i].side_tiles[RIGHT]->id : 0);
-            // printf("  - %zu \n", tiles[i].side_tiles[BOTTOM] ? tiles[i].side_tiles[BOTTOM]->id : 0);
-            // printf("  - %zu \n", tiles[i].side_tiles[LEFT] ? tiles[i].side_tiles[LEFT]->id : 0);
-            // printf("  - count: %zu\n", count);
+            for (auto side : SIDES)
+                if (tiles[t].side_tiles[side] != nullptr)
+                    ++adjacent_n;
 
-            if (count == 2)
-            {
-                corners[idx++] = &tiles[i];
-            }
+            if (adjacent_n == 2)
+                corners[idx++] = &tiles[t];
         }
 
         return corners;
-    }
-
-    [[maybe_unused]]
-    void print_id_map(const std::vector<tile_t>& tiles)
-    {
-        const tile_t* left_top_corner = nullptr;
-        for (size_t i = 0; i < tiles.size(); ++i)
-        {
-            printf("tile: %zu\n", tiles[i].id);
-            printf("  - %zu \n", tiles[i].side_tiles[TOP] ? tiles[i].side_tiles[TOP]->id : 0);
-            printf("  - %zu \n", tiles[i].side_tiles[RIGHT] ? tiles[i].side_tiles[RIGHT]->id : 0);
-            printf("  - %zu \n", tiles[i].side_tiles[BOTTOM] ? tiles[i].side_tiles[BOTTOM]->id : 0);
-            printf("  - %zu \n", tiles[i].side_tiles[LEFT] ? tiles[i].side_tiles[LEFT]->id : 0);
-
-            if (tiles[i].side_tiles[TOP] == nullptr &&
-                tiles[i].side_tiles[RIGHT] != nullptr &&
-                tiles[i].side_tiles[BOTTOM] != nullptr &&
-                tiles[i].side_tiles[LEFT] == nullptr)
-            {
-                left_top_corner = &tiles[i];
-                printf(COLOR_GREEN() "  - left top!\n" COLOR_RESET());
-                break;
-            }
-        }
-
-        // int i = 0;
-
-        // const tile_t* curr_row = left_top_corner;
-        // while (curr_row->side_tiles[BOTTOM] != nullptr)
-        // {
-        //     const tile_t* curr_col = curr_row;
-        //     while (curr_col->side_tiles[RIGHT] != nullptr)
-        //     {
-        //         printf("[ %zu ]", curr_col->id);
-        //         curr_col = curr_col->side_tiles[RIGHT];
-
-        //         if (i > 10)
-        //             return;
-        //         ++i;
-        //     }
-        //     printf("\n");
-
-        //     curr_row = curr_row->side_tiles[BOTTOM];
-        // }
     }
 }
 
 output_t day20(const input_t& input)
 {
     auto tiles = parse_tiles(input);
-    combine_tiles(tiles);
 
     size_t prod = 1;
-    for (auto corner: get_corners(tiles))
+    for (auto* corner: get_corners(tiles))
     {
         prod *= corner->id;
     }
@@ -276,11 +212,9 @@ void day20_test()
     input_t test1 { text1, sizeof(text1) };
 
     auto tiles = parse_tiles(test1);
-    combine_tiles(tiles);
-    // print_id_map(tiles);
 
     size_t prod = 1;
-    for (auto corner: get_corners(tiles))
+    for (auto* corner: get_corners(tiles))
     {
         prod *= corner->id;
     }
