3228a8fa3f4693e83d75ca27176fb95a6b31203b
yanto <yanto@iki.fi>
2021-02-24 20:04:39 +0200

    [day20] combine and rotate the tiles to single orientation

--- src/day20.cpp
+++ src/day20.cpp
@@ -4,6 +4,16 @@ namespace
 {
     enum side_t { TOP, RIGHT, BOTTOM, LEFT };
     constexpr std::array<side_t, 4> SIDES = { TOP, RIGHT, BOTTOM, LEFT };
+    constexpr std::array<std::array<int, 4>, 8> ORIENTATIONS = {
+        std::array<int, 4>{ 0, 1, 2, 3 }, // rot0
+        std::array<int, 4>{ 3, 0, 1, 2 }, // rot90
+        std::array<int, 4>{ 2, 3, 0, 1 }, // rot180
+        std::array<int, 4>{ 1, 2, 3, 0 }, // rot270
+        std::array<int, 4>{ 0, 3, 2, 1 }, // flip0
+        std::array<int, 4>{ 1, 0, 3, 2 }, // flip90
+        std::array<int, 4>{ 2, 1, 0, 3 }, // flip180
+        std::array<int, 4>{ 3, 2, 1, 0 }, // flip270
+    };
 
     struct tile_t
     {
@@ -12,6 +22,7 @@ namespace
         std::array<std::array<bool, 10>, 4> sides = {};
         std::array<std::array<bool, 8>, 8> inner = {};
         std::array<tile_t*, 4> side_tiles = {};
+        bool visited = false;
 
         [[maybe_unused]]
         void print_side(side_t s) const
@@ -25,23 +36,28 @@ namespace
         [[maybe_unused]]
         void print() const
         {
-            printf("ID: %zu\n", id);
+            printf("ID: %zu ", id);
 
-            print_side(TOP);
-            print_side(RIGHT);
-            print_side(BOTTOM);
-            print_side(LEFT);
+            // print_side(TOP);
+            // print_side(RIGHT);
+            // print_side(BOTTOM);
+            // print_side(LEFT);
 
-            printf("Inner:\n");
-            for (int y = 0; y < inner.size(); ++y)
-            {
-                for (int x = 0; x < inner[0].size(); ++x)
-                {
-                    printf("%c", inner[y][x] ? '#' : '.');
-                }
-                printf("\n");
-            }
-            printf("\n");
+            printf(" [TOP   : %4zu]", side_tiles[TOP] ? side_tiles[TOP]->id : 0);
+            printf(" [RIGHT : %4zu]", side_tiles[RIGHT] ? side_tiles[RIGHT]->id : 0);
+            printf(" [BOTTOM: %4zu]", side_tiles[BOTTOM] ? side_tiles[BOTTOM]->id : 0);
+            printf(" [LEFT  : %4zu]\n", side_tiles[LEFT] ? side_tiles[LEFT]->id : 0);
+
+            // printf("Inner:\n");
+            // for (int y = 0; y < inner.size(); ++y)
+            // {
+            //     for (int x = 0; x < inner[0].size(); ++x)
+            //     {
+            //         printf("%c", inner[y][x] ? '#' : '.');
+            //     }
+            //     printf("\n");
+            // }
+            // printf("\n");
         }
     };
 
@@ -155,17 +171,103 @@ namespace
 
         return corners;
     }
+
+    void print_tile_map_ids(tile_t* top_left_corner)
+    {
+        tile_t* row = top_left_corner;
+        while (row != nullptr)
+        {
+            tile_t* col = row;
+            while (col != nullptr)
+            {
+                printf(" %zu ", col->id);
+                col = col->side_tiles[RIGHT];
+            }
+
+            printf("\n");
+            row = row->side_tiles[BOTTOM];
+        }
+    }
+
+    tile_t* get_left_top_corner(const std::array<tile_t*, 4>& corners)
+    {
+        for (auto corner: corners)
+            if (corner->side_tiles[TOP] == nullptr && corner->side_tiles[RIGHT] != nullptr)
+                return corner;
+
+        return nullptr;
+    }
+
+    void orient_tiles(const std::array<tile_t*, 4>& corners)
+    {
+        tile_t* top_left_corner = get_left_top_corner(corners);
+
+        size_t counter = 0;
+        tile_t* tile = top_left_corner;
+        side_t dir = RIGHT;
+
+        while (true)
+        {
+            tile_t* next_tile = tile->side_tiles[dir];
+            if (next_tile != nullptr && !next_tile->visited)
+            {
+                tile->visited = true;
+
+                bool match = false; // safe-guard; there MUST be one orientation that will match
+                for (const auto& o: ORIENTATIONS)
+                {
+                    std::array<tile_t*, 4> sides;
+                    for (size_t i = 0; i < 4; ++i)
+                    {
+                        sides[i] = next_tile->side_tiles[o[i]];
+                    }
+
+                    const side_t op_dir = static_cast<side_t>((dir + 2) % 4); // opposite direction
+                    const side_t hook_dir = static_cast<side_t>((dir + 3) % 4); // hook direction
+
+                    const tile_t* hook1 = tile->side_tiles[hook_dir];
+                    const tile_t* hook2 = hook1 ? hook1->side_tiles[dir] : nullptr;
+
+                    if (tile == sides[op_dir] && hook2 == sides[hook_dir])
+                    {
+                        next_tile->side_tiles = sides;
+                        match = true;
+                    }
+                }
+
+                if (!match)
+                    printf(COLOR_RED() " - didn't find a match!!\n" COLOR_RESET());
+
+                tile = next_tile;
+                counter = 0;
+            }
+            else
+            {
+                dir = static_cast<side_t>((dir + 1) % 4);
+                ++counter;
+            }
+
+            if (counter > 3)
+                break;
+        }
+    }
 }
 
 output_t day20(const input_t& input)
 {
     auto tiles = parse_tiles(input);
+    const auto& corners = get_corners(tiles);
 
     size_t prod = 1;
-    for (auto* corner: get_corners(tiles))
+    for (auto* corner: corners)
     {
         prod *= corner->id;
     }
+
+    orient_tiles(corners);
+
+    tile_t* top_left_corner = get_left_top_corner(corners);
+    print_tile_map_ids(top_left_corner);
 
     return { prod, 0 };
 }
@@ -212,12 +314,23 @@ void day20_test()
     input_t test1 { text1, sizeof(text1) };
 
     auto tiles = parse_tiles(test1);
+    const auto& corners = get_corners(tiles);
 
-    size_t prod = 1;
-    for (auto* corner: get_corners(tiles))
+    // Part 1
     {
-        prod *= corner->id;
+        size_t prod = 1;
+        for (auto* corner: corners)
+        {
+            prod *= corner->id;
+        }
+        assert(prod == 20899048083289);
     }
 
-    assert(prod == 20899048083289);
+    // Part 2
+    {
+        orient_tiles(corners);
+
+        tile_t* top_left_corner = get_left_top_corner(corners);
+        print_tile_map_ids(top_left_corner);
+    }
 }
