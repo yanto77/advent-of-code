433261f4d926dab57737dcc620201c386b5a8bbd
yanto <yanto@iki.fi>
2021-03-06 13:42:09 +0200

    [day23] updated solution

--- src/day23.cpp
+++ src/day23.cpp
@@ -1,27 +1,16 @@
 #include "advent2020.h"
-#include <chrono>
-#include <thread>
-
-using namespace std::chrono_literals;
-
-#define printf(...)
-#define print_row(...)
-constexpr auto SLEEP_AMOUNT = 1ms;
 
 namespace
 {
-    //constexpr size_t CUP_N = 9;
-    typedef std::vector<size_t> cups_t;
-
-    cups_t parse(input_t input)
+    template <typename T>
+    T loop_around(T value, T min, T max)
     {
-        cups_t out;
-        out.reserve(10);
-
-        for (size_t i = 0; input.s[i] != '\n'; ++i)
-            out.push_back((input.s[i] - '0'));
-
-        return out;
+        if (value < min)
+            return max;
+        else if (value > max)
+            return min;
+        else
+            return value;
     }
 
     template <typename T, size_t N>
@@ -34,152 +23,168 @@ namespace
         return false;
     }
 
-    // template <typename T>
-    // bool contains(std::vector<T> data, T value)
-    // {
-    //     for (T val: data)
-    //         if (val == value)
-    //             return true;
-
-    //     return false;
-    // }
-
-    template <typename T>
-    size_t index_of(std::vector<T> data, T value)
+    struct cups_t
     {
-        const size_t N = data.size();
-        for (size_t i = 0; i < N; ++i)
-            if (data[i] == value)
-                return i;
+        std::vector<size_t> links; // { idx: cup idx, val: next cup idx }
 
-        return 0;
+        void set_link(size_t from, size_t to)
+        {
+            links[from] = to;
+        }
+
+        void set_current(size_t cup)
+        {
+            links[0] = cup;
+        }
+
+        size_t get_current() const
+        {
+            return links[0];
+        }
+
+        size_t get_cup_count() const
+        {
+            return links.size() - 1;
+        }
+
+        std::array<size_t, 3> get_pickup() const
+        {
+            size_t first = links[get_current()];
+            size_t second = links[first];
+            size_t third = links[second];
+            return {first, second, third};
+        }
+
+        size_t get_dest_cup(size_t current, const std::array<size_t, 3>& pickup) const
+        {
+            const size_t CUP_N = get_cup_count();
+            size_t dest_cup = loop_around(current - 1, 1LU, CUP_N);
+            while (contains(pickup, dest_cup))
+            {
+                dest_cup = loop_around(dest_cup - 1, 1LU, CUP_N);
+            }
+            return dest_cup;
+        }
+
+        [[maybe_unused]]
+        void print() const
+        {
+            auto cup = get_current();
+            printf("current: %zu (-> %zu)\n", cup, links[cup]);
+            do
+            {
+                printf("%zu, ", cup);
+                cup = links[cup];
+            }
+            while (cup != get_current());
+            printf("(%zu) \n", cup);
+        }
+
+        size_t get_score_part1()
+        {
+            size_t score = 0;
+            const size_t CUP_N = get_cup_count();
+            auto cup = links[1];
+            for (size_t d = 0; d < CUP_N - 1; ++d)
+            {
+                size_t mod = binpow(10, CUP_N - 2 - d);
+                score += (cup * mod);
+                cup = links[cup];
+            }
+
+            return score;
+        }
+
+        size_t get_score_part2()
+        {
+            size_t cup1 = links[1];
+            size_t cup2 = links[cup1];
+            return cup1 * cup2;
+        }
+
+        void grow_to(size_t max_index)
+        {
+            links.reserve(max_index + 1);
+
+            const size_t current_max = get_cup_count();
+
+            // update current loop-around link to continue instead
+            for (size_t idx = 1; idx < links.size(); ++idx)
+            {
+                if (links[idx] == get_current())
+                {
+                    links[idx] = current_max + 1;
+                    break;
+                }
+            }
+
+            // fill in missing entries
+            for (size_t idx = current_max + 1; idx <= max_index; ++idx)
+            {
+                links.push_back(idx+1);
+            }
+
+            // update last link to loop around
+            set_link(max_index, get_current());
+        }
+    };
+
+    cups_t parse(input_t input)
+    {
+        std::vector<size_t> in;
+        in.reserve(10);
+
+        for (size_t i = 0; input.s[i] != '\n'; ++i)
+            in.push_back((input.s[i] - '0'));
+
+        cups_t out;
+        out.links = std::vector<size_t>(10, 0);
+
+        for (size_t i = 0; i < (in.size() - 1); ++i)
+        {
+            out.set_link(in[i], in[i+1]);
+        }
+
+        out.set_link(in.back(), in.front());
+        out.set_current(in.front());
+
+        return out;
     }
 
     cups_t evaluate(cups_t cups, size_t moves)
     {
-        const size_t CUP_N = cups.size();
-        assert(CUP_N != 0);
-
-        cups_t copy = cups;
-        printf("evaluate!\n");
-
         for (size_t move = 0; move < moves; ++move)
         {
-            std::cout << "move: " << move << "\t\t\t\r";
-            copy = cups;
-            size_t idx = move % CUP_N;
-            printf("-- move %zu --\n", move + 1);
+            auto current_cup = cups.get_current();
+            auto pickup_cups = cups.get_pickup();
+            auto dest_value = cups.get_dest_cup(current_cup, pickup_cups);
 
-            printf("cups: "); print_row(cups);
-            printf("current: %lu\n", cups[idx]);
-
-            std::array<size_t, 3> pickup {};
-            for (size_t i = 0; i < 3; ++i)
-            {
-                size_t read_idx = (idx + 1 + i) % CUP_N;
-                pickup[i] = cups[read_idx];
-            }
-            printf("pickup: "); print_row(pickup);
-
-            size_t dest_value = (cups[idx] - 1);
-            if (dest_value <= 0) dest_value = CUP_N;
-
-            printf(" .. dest_value? %lu\n", dest_value);
-            while (contains(pickup, dest_value))
-            {
-                --dest_value;
-                if (dest_value <= 0) dest_value = CUP_N;
-                printf(" .. dest_value? %lu\n", dest_value);
-            }
-            printf("dest_value: %lu\n", dest_value);
-
-            // Starting from idx:
-            size_t read_idx = idx;
-            size_t write_idx = idx;
-
-            auto advance_both_idx = [&]()
-            {
-                read_idx = (read_idx + 1) % CUP_N;
-                if (cups[read_idx] == pickup[0])
-                    read_idx = (read_idx + 3) % CUP_N;
-                write_idx = (write_idx + 1) % CUP_N;
+            std::array<size_t, 3> prev = {
+                cups.links[current_cup],
+                cups.links[dest_value],
+                cups.links[pickup_cups[2]]
             };
 
-            // 1. write down current to idx
-            printf("- 1. reading (%lu), writing to %zu\n", cups[read_idx], write_idx);
-            copy[write_idx] = cups[read_idx];
-            advance_both_idx();
+            cups.links[current_cup] = prev[2];
+            cups.links[dest_value] = prev[0];
+            cups.links[pickup_cups[2]] = prev[1];
 
-            // 2. pre-destination
-            while (cups[read_idx] != dest_value)
-            {
-                printf("- 2. reading (%lu), writing to %zu\n", cups[read_idx], write_idx);
-                copy[write_idx] = cups[read_idx];
-                advance_both_idx();
-                std::this_thread::sleep_for(SLEEP_AMOUNT);
-            }
-
-            // 2.1 destination itself
-            printf("- 2.1 reading (%lu), writing to %zu\n", cups[read_idx], write_idx);
-            copy[write_idx] = dest_value;
-            advance_both_idx();
-
-            // 3. pickup cups (3 values)
-            for (auto picked_value: pickup)
-            {
-                printf("- 3. reading pickup (%lu), writing to %zu\n", picked_value, write_idx);
-                copy[write_idx] = picked_value;
-                write_idx = (write_idx + 1) % CUP_N;
-                std::this_thread::sleep_for(SLEEP_AMOUNT);
-            }
-
-            // 4. post-destination (and pickup cups)
-            while (cups[read_idx] != cups[idx])
-            {
-                printf("- 4. reading (%lu), writing to %zu\n", cups[read_idx], write_idx);
-                copy[write_idx] = cups[read_idx];
-                advance_both_idx();
-                std::this_thread::sleep_for(SLEEP_AMOUNT);
-            }
-
-            printf("-> result: "); print_row(copy);
-            cups = copy;
+            cups.set_current(cups.links[current_cup]);
         }
-
-        std::cout << "\nend!\n";
 
         return cups;
     }
-
-    size_t get_part1_score(const cups_t& cups)
-    {
-        const size_t CUP_N = cups.size();
-        size_t score = 0;
-        size_t idx = index_of(cups, 1LU) + 1;
-        for (size_t d = 0; d < CUP_N - 1; ++d)
-        {
-            size_t val = cups[(idx + d) % CUP_N];
-            size_t mod = binpow(10, CUP_N - 2 - d);
-            printf("score += %zu * %zu\n", val, mod);
-            score += (val * mod);
-        }
-
-        return score;
-    }
-
-    // size_t get_part2_score()
-    // {
-    //     return 0;
-    // }
 }
 
 output_t day23(const input_t& input)
 {
     auto out = parse(input);
-    auto cups = evaluate(out, 100);
-    size_t part1 = get_part1_score(cups);
+    size_t part1 = evaluate(out, 100).get_score_part1();
 
-    return { part1, 0 };
+    out.grow_to(1000000);
+    size_t part2 = evaluate(out, 10000000).get_score_part2();
+
+    return { part1, part2 };
 }
 
 void day23_test()
@@ -188,8 +193,10 @@ void day23_test()
     input_t test1 { text1, sizeof(text1) };
 
     auto out = parse(test1);
-    // print_row(out);
 
-    // assert(92658374 == get_part1_score(evaluate(out, 10)));
-    // assert(67384529 == get_part1_score(evaluate(out, 100)));
+    assert(92658374 == evaluate(out, 10).get_score_part1());
+    assert(67384529 == evaluate(out, 100).get_score_part1());
+
+    out.grow_to(1000000);
+    assert(149245887792 == evaluate(out, 10000000).get_score_part2());
 }
