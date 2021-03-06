d2892db1fc21a27ea225f236f3642e28c5bf83c8
yanto <yanto@iki.fi>
2021-03-06 19:01:17 +0200

    [day25] add final solution, 50 stars, woo \o/

--- src/day25.cpp
+++ src/day25.cpp
@@ -1,10 +1,48 @@
 #include "advent2020.h"
 
+namespace
+{
+    constexpr size_t transform(size_t subject, size_t n_loops)
+    {
+        size_t value = 1;
+        for (size_t idx = 0; idx < n_loops; ++idx)
+        {
+            value = (value * subject) % 20201227;
+        }
+        return value;
+    }
+
+    constexpr size_t get_loop_size(size_t input)
+    {
+        size_t loop_idx = 1;
+        size_t value = 1;
+        while (true)
+        {
+            value = (value * 7) % 20201227;
+
+            if (value == input)
+                return loop_idx;
+
+            ++loop_idx;
+            if (loop_idx > 100000000)
+                break;
+        }
+
+        return 0;
+    }
+}
+
 output_t day25(const input_t& input)
 {
-    return { 0, 0 };
+    size_t key1 = transform(8335663, get_loop_size(8614349));
+    size_t key2 = transform(8614349, get_loop_size(8335663));
+    assert(key1 == key2);
+    return { key1, 1 };
 }
 
 void day25_test()
 {
+    static_assert(8 == get_loop_size(5764801));
+    static_assert(11 == get_loop_size(17807724));
+    static_assert(14897079 == transform(17807724, get_loop_size(5764801)));
 }
