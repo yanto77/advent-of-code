fce1a1d360eff1d16cf8aa5102ec30d7a75aa364
yanto <yanto@iki.fi>
2021-04-25 16:07:13 +0300

    [day03] replace inner std::array with single uint32_t

--- src/day03.cpp
+++ src/day03.cpp
@@ -1,28 +1,24 @@
 #include "advent2020.h"
 
-constexpr static size_t COL_N = 31;
-constexpr static size_t ROW_N = 323;
+static constexpr size_t COL_N = 31;
+static constexpr size_t ROW_N = 323;
 
-typedef std::array<std::array<bool, COL_N>, ROW_N> tree_map_t;
+typedef std::array<uint32_t, ROW_N> tree_map_t;
 
 namespace
 {
     tree_map_t get_tree_map(const input_t& input)
     {
-        std::array<std::array<bool, COL_N>, ROW_N> map;
-        for (auto& row : map)
-        {
-            row.fill(false);
-        }
+        tree_map_t map {};
 
-        size_t current_row = 0;
+        size_t row = 0;
         parse_input(input, [&](const sv& line)
         {
             for (size_t c = 0; c < line.size(); ++c)
                 if (line[c] == '#')
-                    map[current_row][c] = true;
+                    set_bit(map[row], c);
 
-            ++current_row;
+            ++row;
         });
 
         // with given input, there should be 1934 trees.
@@ -33,17 +29,16 @@ namespace
     {
         vec2i pos { 0, 0 };
 
-        int trees_encountered = 0;
+        int trees = 0;
         while (pos.y < ROW_N)
         {
-            // printf("current pos: col %d, row %d\n", pos.y, pos.x);
-            if (map[pos.y][pos.x % COL_N])
-                ++trees_encountered;
+            if (get_bit(map[pos.y], pos.x % COL_N))
+                ++trees;
 
             pos += slope;
         }
 
-        return trees_encountered;
+        return trees;
     }
 }
 
@@ -51,10 +46,7 @@ output_t day03(const input_t& input)
 {
     const tree_map_t& map = get_tree_map(input);
 
-    // Part 1
     size_t part1 = get_encountered_trees(map, { 3, 1 });
-
-    // Part 2
     size_t part2 = part1;
     part2 *= get_encountered_trees(map, vec2i { 1, 1 });
     part2 *= get_encountered_trees(map, vec2i { 5, 1 });
