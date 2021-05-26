a4a97c95252eaf5cf5a65ca3c3ceaee70b015148
yanto <yanto@iki.fi>
2021-05-26 23:12:15 +0300

    add 2019 day1 solution

--- src/2019/day01.cpp
+++ src/2019/day01.cpp
@@ -1,16 +1,47 @@
 #include "setup/all.h"
 
-ADVENT_DAY(2019, 1, 0, 0);
+ADVENT_DAY(2019, 1, 3520097, 5277255);
 
 namespace
 {
+    constexpr int32_t fn1(int32_t in)
+    {
+        return (in / 3) - 2;
+    }
+
+    constexpr int32_t fn2(int32_t in)
+    {
+        auto out = (in / 3) - 2;
+        if (out > 0)
+            return out + fn2(out);
+        else
+            return 0;
+    }
 }
 
 void Day_2019_1::run_tests() const
 {
+    static_assert(fn1(12) == 2);
+    static_assert(fn1(14) == 2);
+    static_assert(fn1(1969) == 654);
+    static_assert(fn1(100756) == 33583);
+
+    static_assert(fn2(12) == 2);
+    static_assert(fn2(14) == 2);
+    static_assert(fn2(1969) == 966);
+    static_assert(fn2(100756) == 50346);
 }
 
-output_t Day_2019_1::run_solution(const input_t& /* input */) const
+output_t Day_2019_1::run_solution(const input_t& input) const
 {
-    return {0, 0};
+    size_t sum1 = 0;
+    size_t sum2 = 0;
+    parse_input(input, [&](const sv& line)
+    {
+        auto value = to_int<int32_t>(line);
+        sum1 += fn1(value);
+        sum2 += fn2(value);
+    });
+
+    return {sum1, sum2};
 }
