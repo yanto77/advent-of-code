f0fc5f26959159a21f0ce7d3f87acbf4318bcb57
yanto <yanto@iki.fi>
2021-05-26 20:48:29 +0300

    add placeholders for 2019 day01

---
+++ src/2019/day01.cpp
@@ -0,0 +1,16 @@
+#include "setup/all.h"
+
+ADVENT_DAY(2019, 1, 0, 0);
+
+namespace
+{
+}
+
+void Day_2019_1::run_tests() const
+{
+}
+
+output_t Day_2019_1::run_solution(const input_t& /* input */) const
+{
+    return {0, 0};
+}
