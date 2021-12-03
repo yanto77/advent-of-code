24c4b69f4156942ef51db933e027498f8ff29ab4
yanto <yanto@iki.fi>
2021-12-03 11:36:08 +0200

    add initial stubs for 2021

---
+++ src/2021/day01.cpp
@@ -0,0 +1,15 @@
+#include "setup/all.h"
+
+ADVENT_DAY(2021, 1, 0, 0);
+
+output_t Day_2021_1::run_solution(const input_t& input) const
+{
+    (void)input;
+    size_t part1 = 0;
+    size_t part2 = 0;
+    return { part1, part2 };
+}
+
+void Day_2021_1::run_tests() const
+{
+}
---
+++ src/2021/day02.cpp
@@ -0,0 +1,15 @@
+#include "setup/all.h"
+
+ADVENT_DAY(2021, 2, 0, 0);
+
+output_t Day_2021_2::run_solution(const input_t& input) const
+{
+    (void)input;
+    size_t part1 = 0;
+    size_t part2 = 0;
+    return { part1, part2 };
+}
+
+void Day_2021_2::run_tests() const
+{
+}
