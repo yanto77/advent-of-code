0437a3b76dc82da94f865d9385e3619db44c83ad
yanto <yanto@iki.fi>
2022-12-02 17:56:43 +0200

    add solution for 02/2022

---
+++ src/2022/day02.cpp
@@ -0,0 +1,65 @@
+#include "setup/all.h"
+
+ADVENT_DAY(2022, 2, 11666, 12767);
+
+namespace 
+{
+    enum Hand { Rock = 0, Paper = 1, Scissors = 2 };
+    enum Result { Lose = 0, Draw = 1, Win = 2 };
+    struct Round { 
+        Hand opponent; 
+        union { Hand you; Result result; };
+    };
+
+    // Combined scores for single round:
+    //   - the shape you selected (Rock=1, Paper=2, Scissors=3) 
+    //   - the outcome of the round (lost=0, draw=3, won=6).
+    static const uint8_t s_points[9] = {
+        (1+3), (2+6), (3+0),
+        (1+0), (2+3), (3+6),
+        (1+6), (2+0), (3+3)
+    };
+
+    // Strategy based on opponents hand (horizontal) + intended result (vertical).
+    static const Hand s_strategies[9] = {
+        (Hand::Scissors), (Hand::Rock),     (Hand::Paper),
+        (Hand::Rock),     (Hand::Paper),    (Hand::Scissors),
+        (Hand::Paper),    (Hand::Scissors), (Hand::Rock)
+    };
+
+    Round parse_line(const sv& line)
+    {
+        Round round;
+
+        if (line[0] == 'A') round.opponent = Hand::Rock;
+        if (line[0] == 'B') round.opponent = Hand::Paper;
+        if (line[0] == 'C') round.opponent = Hand::Scissors;
+
+        if (line[2] == 'X') round.you = Hand::Rock;
+        if (line[2] == 'Y') round.you = Hand::Paper;
+        if (line[2] == 'Z') round.you = Hand::Scissors;
+
+        return round;
+    }
+}
+
+output_t Day_2022_2::run_solution(const input_t& input) const
+{
+    size_t part1 = 0;
+    size_t part2 = 0;
+
+    parse_input(input, [&](const sv& line)
+    {
+        Round round = parse_line(line);
+        part1 += s_points[(round.opponent * 3) + round.you];
+
+        Hand pt2_strategy = s_strategies[(round.opponent * 3) + round.result];
+        part2 += s_points[(round.opponent * 3) + pt2_strategy];
+    });
+
+    return { part1, part2 };
+}
+
+void Day_2022_2::run_tests() const
+{
+}
