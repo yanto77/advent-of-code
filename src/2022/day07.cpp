febc4d58a934d27b5231374870c27b7c952c2986
yanto <yanto@iki.fi>
2022-12-09 19:11:25 +0200

    improve 07/2022 (210 -> 20 us)

--- src/2022/day07.cpp
+++ src/2022/day07.cpp
@@ -5,57 +5,39 @@ ADVENT_DAY(2022, 7, 1427048, 2940614);
 
 namespace 
 {
-    using filesystem_t = std::unordered_map<std::string, size_t>;
-
-    filesystem_t parse_logs(str_view input)
+    // returns vector of sizes, where each index corresponds to each unique dir.
+    // (e.g., dirs[0] is "/", dirs[1] is "/abc", dirs[2] is "/abc/def", etc.)
+    std::vector<size_t> parse_logs(str_view input)
     {
-        filesystem_t fs;
-        std::vector<str_view> cwd; // TODO: stack
+        std::vector<size_t> cwd; // stack of indexes into `dirs`
+        std::vector<size_t> dirs;
+        dirs.reserve(256); // input has 159 dirs
 
         for_each_split(input, '\n', [&](str_view line)
         {
             if (line.starts_with("$ cd"))
             {
-                str_view arg = line.substr(5);
-                if (arg == "..") 
+                if (line.substr(5) == "..") 
+                {
                     cwd.pop_back();
-                else
-                    cwd.push_back(arg);
-
-                std::string fullpath;
-                for (auto part: cwd)
-                {
-                    fullpath.append("/");
-                    fullpath.append(part);
                 }
-
-                if (!fs.contains(fullpath))
+                else 
                 {
-                    fs.insert({ fullpath, 0});
+                    dirs.push_back(0); // new dir entry
+                    cwd.push_back(dirs.size() - 1); // new dir index
                 }
             }
-            else if (line.starts_with("$ ls") || line.starts_with("dir "))
+            else if (!line.starts_with("$ ls") && !line.starts_with("dir ")) // it's a file
             {
-                // ignore
-            }
-            else // it's a file
-            {
-                uint32_t file_size = 0;
-                parse_uint_scalar(line, [&](uint32_t num) { file_size = num; });
-                
-                std::string parentpath;
-                for (auto part: cwd)
+                const uint32_t file_size = to_int<uint32_t>(line);
+                for (size_t index: cwd) // file's size is added to each parent
                 {
-                    parentpath.append("/");
-                    parentpath.append(part);
-
-                    size_t& parent_size = fs.find(parentpath)->second;
-                    parent_size += file_size;
+                    dirs[index] += file_size;
                 }
             }
         });
 
-        return fs;
+        return dirs;
     }
 }
 
@@ -64,23 +46,13 @@ result_t Day_2022_7::run_solution(str_view input) const
     size_t part1 = 0;
     size_t part2 = SIZE_MAX;
 
-    // total dir number is 159
-    filesystem_t fs = parse_logs(input);
+    std::vector<size_t> dirs = parse_logs(input);
 
-    size_t free = 70000000 - (fs.find("//")->second);
-    size_t need = 30000000;
-    size_t need_more = need - free;
-    for (auto [name, size]: fs)
+    const size_t need_more = dirs[0] - 40000000; // dirs[0] is the size of '/'
+    for (auto size: dirs)
     {
-        if (size <= 100000)
-        {
-            part1 += size;
-        }
-        
-        if (size > need_more)
-        {
-            part2 = min(part2, size);
-        }
+        if (size <= 100000) part1 += size;
+        if (size > need_more) part2 = min(part2, size);
     }
 
     return { part1, part2 };
