b47d39850f91cac15719504054ff255d21f65c2e
yanto <yanto@iki.fi>
2022-12-27 14:16:29 +0200

    add range-based solution for 15/2022

--- src/2022/day15.cpp
+++ src/2022/day15.cpp
@@ -1,209 +1,207 @@
 #include "setup/all.h"
 #include <optional>
+#include <unordered_set>
 
-ADVENT_DAY(2022, 15, 5367037, 0);
+ADVENT_DAY(2022, 15, 5367037, 11914583249288);
 
 namespace 
 {
-    struct sensor_t 
-    {
-        vec2i pos {};
-        vec2i beacon {};
-    };
-
     struct bound_t 
     {
         vec2i min {INT32_MAX, INT32_MAX};
         vec2i max {INT32_MIN, INT32_MIN};
+
+        void add(vec2i p)
+        {
+            min.x = ::min(min.x, p.x);
+            min.y = ::min(min.y, p.y);
+            max.x = ::max(max.x, p.x);
+            max.y = ::max(max.y, p.y);
+        }
     };
 
-    std::vector<sensor_t> parse_sensors(str_view input)
+    struct sensor_t { vec2i center; int64_t extent; };
+    
+    struct sensor_map_t 
     {
-        std::vector<sensor_t> result;
+        bound_t outer_bound;
+        bound_t inner_bound;
+        std::vector<sensor_t> sensors;
+        std::unordered_set<vec2i> beacons;
 
-        for_each_split(input, '\n', [&](str_view line)
+        sensor_map_t(str_view input)
         {
-            // fmt::print("line: {}\n", line);
-            auto [sx, next1] = to_int<int32_t>(line, sizeof("Sensor at x=")-1);
-            auto [sy, next2] = to_int<int32_t>(line, next1 + sizeof(", y=")-1);
-            auto [bx, next3] = to_int<int32_t>(line, next2 + sizeof(": closest beacon is at x=")-1);
-            auto [by, next4] = to_int<int32_t>(line, next3 + sizeof(", y=")-1);
-            // fmt::print(" - {}, {}, {}, {}\n", sx, sy, bx, by);
-            result.push_back(sensor_t{ vec2i{sx, sy}, vec2i{bx, by} });
-        });
-
-        return result;
-    }
-
-    bound_t get_total_bound(const std::vector<sensor_t>& sensors)
-    {
-        bound_t total {};
-        for (auto [sensor, beacon]: sensors)
-        {
-            int32_t dist = (int32_t)dist_mnht(sensor, beacon);
-            vec2i p1 { sensor.x - dist, sensor.y - dist };
-            total.min.x = min(total.min.x, p1.x);
-            total.min.y = min(total.min.y, p1.y);
-            vec2i p2 { sensor.x + dist, sensor.y + dist };
-            total.max.x = max(total.max.x, p2.x);
-            total.max.y = max(total.max.y, p2.y);
-        }
-
-        fmt::print("bound: min {} {}, max: {} {}\n", total.min.x, total.min.y, total.max.x, total.max.y);
-        return total;
-    }
-
-    bool is_cell_covered(const std::vector<sensor_t>& sensors, vec2i pos)
-    {
-        for (auto [sensor, beacon]: sensors)
-        {
-            if (pos == sensor || pos == beacon)
-                continue;
-
-            if (dist_mnht(sensor, pos) <= dist_mnht(sensor, beacon))
-                return true;
-        }
-        
-        return false;
-    }
-
-    void print_map(const std::vector<sensor_t>& sensors, std::optional<vec2i> center = {})
-    {
-        auto [min, max] = get_total_bound(sensors);
-        if (center.has_value())
-        {
-            min.x = center.value().x - 5;
-            min.y = center.value().y - 5;
-            max.x = center.value().x + 5;
-            max.y = center.value().y + 5;
-        }
-
-        for (int32_t y = min.y; y <= max.y; y++)
-        {
-            for (int32_t x = min.x; x <= max.x; x++)
+            for_each_split(input, '\n', [&](str_view line)
             {
-                vec2i pos { x, y };
-                bool found_sensor = false;
-                bool found_beacon = false;
-                bool in_range = false;
+                auto [sx, next1] = to_int<int32_t>(line, sizeof("Sensor at x=")-1);
+                auto [sy, next2] = to_int<int32_t>(line, next1 + sizeof(", y=")-1);
+                auto [bx, next3] = to_int<int32_t>(line, next2 + sizeof(": closest beacon is at x=")-1);
+                auto [by, next4] = to_int<int32_t>(line, next3 + sizeof(", y=")-1);
 
-                for (auto [sensor, beacon]: sensors)
-                {
-                    if (pos == sensor) found_sensor = true;
-                    if (pos == beacon) found_beacon = true;
+                vec2i sensor {sx, sy};
+                vec2i beacon {bx, by};
+                int32_t extent = (int32_t)dist_mnht(sensor, beacon);
+                sensors.push_back({ sensor, extent });
+                beacons.insert({ beacon });
 
-                    // Print sensor range
-                    int32_t dist1 = (int32_t)dist_mnht(sensor, pos);
-                    int32_t dist2 = (int32_t)dist_mnht(sensor, beacon);
-                    in_range |= dist1 <= dist2;
-
-                    // Print sensor range AABB
-                    // vec2i p1 { sensor.x - dist2, sensor.y - dist2 };
-                    // vec2i p2 { sensor.x + dist2, sensor.y + dist2 };
-                    // in_range |= (p1 <= pos && pos <= p2);
-                }
-
-                if (found_sensor)       fmt::print(COLOR_BLUE() "S");
-                else if (found_beacon)  fmt::print(COLOR_GREEN() "B");
-                else if (in_range)      fmt::print(COLOR_WHITE() "#");
-                else                    fmt::print(COLOR_GRAY() ".");
-            }
-            fmt::print(COLOR_RESET() "\n");
-        }
-    }
-
-    // return number of occupied cells across single y-line
-    size_t count_line_pos(const std::vector<sensor_t>& sensors, int32_t y)
-    {
-        size_t n_pos = 0;
-
-        auto [min, max] = get_total_bound(sensors);
-        for (int32_t x = min.x; x <= max.x; x++)
-        {
-            n_pos += (int)is_cell_covered(sensors, {x, y});
+                outer_bound.add(vec2i { sx - extent, sy - extent });
+                outer_bound.add(vec2i { sx + extent, sy + extent });
+                inner_bound.add(vec2i { sx, sy });
+            });
         }
 
-        return n_pos;
-    }
-
-    // find beacon pos (unoccupied cell in between of all sensor-areas)
-    vec2i find_beacon(const std::vector<sensor_t>& sensors)
-    {
-        auto [min, max] = get_total_bound(sensors);
-        min.x = ::max(min.x, 0);
-        min.y = ::max(min.y, 0);
-        max.x = ::min(max.x, 4000000);
-        max.y = ::min(max.y, 4000000);
-        // fmt::print("test part2: {}, pos: {}, {}\n", part2, asd.x, asd.y);
-
-        for (int32_t y = min.y; y <= max.y; y++)
+        size_t count_covered_cells(int32_t y)
         {
-            for (int32_t x = min.x; x <= max.x; x++)
+            size_t count = 0;
+
+            for (auto [x1, x2]: compute_ranges(y, outer_bound))
             {
-                fmt::print("x%: {:2.3f}, y%: {:2.3f}\n", (float)x / (max.x - min.x), (float)y / (max.y - min.y));
-                // 0 1 2
-                // 3 4 5
-                // 6 7 8
-                bool cell0 = is_cell_covered(sensors, {x-1, y-1});
-                bool cell1 = is_cell_covered(sensors, {x+0, y-1});
-                bool cell2 = is_cell_covered(sensors, {x+1, y-1});
-                bool cell3 = is_cell_covered(sensors, {x-1, y+0});
-                bool cell4 = is_cell_covered(sensors, {x+0, y+0});
-                bool cell5 = is_cell_covered(sensors, {x+1, y+0});
-                bool cell6 = is_cell_covered(sensors, {x-1, y+1});
-                bool cell7 = is_cell_covered(sensors, {x+0, y+1});
-                bool cell8 = is_cell_covered(sensors, {x+1, y+1});
-
-                bool beacon = 
-                    cell0 && cell1 && cell2 &&
-                    cell3 && !cell4 && cell5 &&
-                    cell6 && cell7 && cell8;
-
-                if (beacon)
+                count += (x2 - x1) + 1; // NB: incl. range
+                for (vec2i beacon: beacons)
                 {
-                    bool taken = false;
-                    for (auto [sensor, beacon]: sensors)
+                    if (y == beacon.y && x1 <= beacon.x && beacon.x <= x2)
                     {
-                        if (sensor == vec2i{x, y} || beacon == vec2i{x, y}) 
-                        {
-                            taken = true;
-                            break;
+                        count--;
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        size_t find_beacon_score()
+        {
+            for (int32_t y = inner_bound.min.y; y < inner_bound.max.y; y++)
+            {
+                auto ranges = compute_ranges(y, inner_bound);
+                if (ranges.size() == 2)
+                {
+                    const vec2i a = ranges[0], b = ranges[1];
+                    if (a.x > b.y) return int64_t(b.y + 1) * 4000000 + y;
+                    else return int64_t(a.y + 1) * 4000000 + y;
+                }
+            }
+
+            assert(false);
+            return 0;
+        }
+
+        [[maybe_unused]]
+        void print(std::optional<vec2i> center = {}, std::vector<vec2i> highlights = {})
+        {
+            auto [min, max] = inner_bound;
+            if (center.has_value())
+            {
+                min.x = center.value().x - 5;
+                min.y = center.value().y - 5;
+                max.x = center.value().x + 5;
+                max.y = center.value().y + 5;
+            }
+
+            for (int32_t y = min.y; y <= max.y; y++)
+            {
+                fmt::print("{:3} : ", y);
+                for (int32_t x = min.x; x <= max.x; x++)
+                {
+                    const vec2i pos { x, y };
+
+                    // Layer 1: Beacons and Sensors
+                    for (auto beacon: beacons) {
+                        if (pos == beacon) {
+                            fmt::print(COLOR_GREEN() "B");
+                            goto next;
+                        }
+                    }
+                    for (auto [sensor, extent]: sensors) {
+                        if (pos == sensor) { 
+                            fmt::print(COLOR_BLUE() "S");
+                            goto next;
                         }
                     }
 
-                    if (!taken)
+                    // Layer 2: High-lighted areas
+                    for (auto [sensor, extent]: sensors) 
                     {
-                        print_map(sensors, {{x, y}});
-                        return {x, y};
+                        if (dist_mnht(sensor, pos) <= extent) // in range
+                        {
+                            for (auto highlight: highlights) 
+                            {
+                                if (sensor == highlight) 
+                                {
+                                    fmt::print(COLOR_CYAN() "#");
+                                    goto next;
+                                }
+                            }
+                        }
                     }
+
+                    // Layer 3: Sensor areas (not highlighted)
+                    for (auto [sensor, extent]: sensors) 
+                    {
+                        if (dist_mnht(sensor, pos) <= extent) // in range
+                        {
+                            fmt::print(COLOR_GRAY() "#");
+                            goto next;
+                        }
+                    }
+
+                    // Layer 4: Empty locations
+                    fmt::print(COLOR_GRAY() ".");
+
+                    next:
+                        continue;
                 }
+                fmt::print(COLOR_RESET() "\n");
             }
         }
 
-        return {0, 0};
-    }
+        private:
+            std::vector<vec2i> compute_ranges(int32_t y, bound_t bound)
+            {
+                std::vector<vec2i> ranges;
+                for (auto [center, extent]: sensors)
+                {
+                    int32_t extent_in_y = (int32_t)extent - abs(y - center.y);
+                    if (extent_in_y > 0)
+                    {
+                        int32_t x1 = clamp(center.x - extent_in_y, bound.min.x, bound.max.x);
+                        int32_t x2 = clamp(center.x + extent_in_y, bound.min.x, bound.max.x);
+                        ranges.push_back(vec2i{x1, x2});
+                    }
+                }
+
+                for (int64_t i = ranges.size() - 1; i >= 0; i--)
+                {
+                    for (int64_t j = i - 1; j >= 0; j--)
+                    {
+                        const vec2i a = ranges[i], b = ranges[j];
+                        if (!((b.x - 1) > a.y || (a.x - 1) > b.y))
+                        {
+                            ranges[j] = { min(a.x, b.x), max(a.y, b.y) };
+                            ranges[i] = std::move(ranges.back());
+                            ranges.pop_back();
+                            break;
+                        }
+                    }
+                }
+
+                return ranges;
+            }
+    };
 }
 
-// TODO: add bvh for log-n checks?
 result_t Day_2022_15::run_solution(str_view input) const
 {
-    size_t part1 = 0;
-    size_t part2 = 0;
+    sensor_map_t map(input);
+    size_t part1 = map.count_covered_cells(2000000);
+    size_t part2 = map.find_beacon_score();
 
-    // std::vector<sensor_t> sensors = parse_sensors(input);
-    // // print_map(sensors);
-
-    // part1 = count_line_pos(sensors, 2000000);
-    // auto asd = find_beacon(sensors);
-    // part2 = 4000000 * asd.x + asd.y;
-
-    // must have x and y coordinates each no lower than 0 and no larger than 4000000.
+    assert(part2 > 343970184);
     return { part1, part2 };
 }
 
 void Day_2022_15::run_tests() const
 {
-
     std::string test_input = 
         "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\n"
         "Sensor at x=9, y=16: closest beacon is at x=10, y=16\n"
@@ -220,15 +218,28 @@ void Day_2022_15::run_tests() const
         "Sensor at x=14, y=3: closest beacon is at x=15, y=3\n"
         "Sensor at x=20, y=1: closest beacon is at x=15, y=3\n";
 
-    std::vector<sensor_t> sensors = parse_sensors(test_input);
-    // print_map(sensors);
-
-    size_t part1 = count_line_pos(sensors, 10);
+    sensor_map_t map(test_input);
+    size_t part1 = map.count_covered_cells(10);
     assert(part1 == 26);
-    fmt::print("test part1: {}\n", part1);
+    // fmt::print("test part1: {}\n", part1);
 
-    auto asd = find_beacon(sensors);
-    size_t part2 = 4000000 * asd.x + asd.y;
-    fmt::print("test part2: {}, pos: {}, {}\n", part2, asd.x, asd.y);
+    // map.print();
+    size_t part2 = map.find_beacon_score();
     assert(part2 == 56000011);
+    // fmt::print("test part2: {}\n", part2);
+
+    // for (size_t i = 0; i < map.sensors.size(); i++)
+    // {
+    //     for (size_t j = 0; j < map.sensors.size() && i != j; j++)
+    //     {
+    //         auto [ap, ad] = map.sensors[i];
+    //         auto [bp, bd] = map.sensors[j];
+    //         if (dist_mnht(ap, bp) - bd - ad == 2)
+    //         {
+    //             map.print({}, { ap, bp });
+    //             fmt::print("- {} and {}: ranges: {}, {}\n", i, j, ad, bd);
+    //             fmt::print("\n");
+    //         }
+    //     }
+    // }
 }
